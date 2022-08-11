da4ba3c598739e27e256afd70fde7da374c19a0b
faniAhmed <89826402+faniAhmed@users.noreply.github.com>
2022-08-11 08:07:20 +0500

    Update main.py

--- main.py
+++ main.py
@@ -21,9 +21,14 @@ def cricket():
     res_lst = []
     for match in match_lst:
         try:
-            sc = match["SC"]["S"]#score
-        except:
-            sc = "0"
+            sc = match["SC"]["FS"]#score
+        except Exception as e:
+            print(e)
+        if sc == {}:
+            sc = {
+                "S1":"0",
+                "S2":"0"
+                }
         l = match["L"]#league
         t1 = match["O1"]#T1
         t2 = match["O2"]#T2
