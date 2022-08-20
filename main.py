0c032d88a860120935e39bd6e78736085e2669e8
faniAhmed <89826402+faniAhmed@users.noreply.github.com>
2022-08-20 13:25:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,55 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
+@app.get("/cricket2")
+def cricket2():
+    params = {
+        'sports': '66',
+        'count': '50',
+        'lng': 'en',
+        'getEmpty': 'true',
+        'noFilterBlockEvent': 'true',
+    }
+
+
+    response = requests.get('https://linebet.com/LiveFeed/Get1x2_VZip', params=params)
+    rj =response.json()
+    match_lst = rj["Value"]
+    res_lst = []
+    for match in match_lst:
+        sc = match["SC"]["FS"]#score
+        if sc == {}:
+            sc = {"S1":0,"S2":0}
+        if "S1" not in sc:
+            sc["S1"] = 0
+        if "S2" not in sc:
+            sc["S2"] = 0
+        l = match["L"]#league
+        t1 = match["O1"]#T1
+        t2 = match["O2"]#T2
+        try:
+            loc = match["MIO"]["Loc"]
+        except:
+            try:
+                loc = match["CN"]
+            except:
+                loc = ""
+        try:
+            Type = match["MIO"]["MaF"]
+        except:
+            Type = ""
+        cj = {
+            "score":sc,
+            "league":l,
+            "team1":t1,
+            "team2":t2,
+            "location":loc,
+            "type":Type,
+        }
+        res_lst.append(cj)
+    return {"Result" : res_lst}
+
 @app.get("/cricket")
 def cricket():
     headers = {
