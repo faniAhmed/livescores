e7075c0d5cf9ce2f481592034d78e922c0663c28
faniAhmed <89826402+faniAhmed@users.noreply.github.com>
2022-08-24 07:57:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,123 @@
 import requests
 from fastapi import FastAPI
+from bs4 import BeautifulSoup as soap
+import datetime
 
 app = FastAPI()
+
+@app.get("/upcoming")
+def upcoming():
+    date = datetime.datetime.now()    
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        # 'Accept-Encoding': 'gzip, deflate, br',
+        'DNT': '1',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'none',
+        'Sec-Fetch-User': '?1',
+    }
+
+    response = requests.get('https://sports.ndtv.com/cricket/schedules-fixtures', headers=headers)
+    page = soap(response.text, "lxml")
+    aaj = page.find_all("div", {"id": date.strftime("%d-%m-20%y")})
+    kal = page.find_all("div", {"id": (date + datetime.timedelta(days=1)).strftime("%d-%m-20%y")})
+    parson =page.find_all("div", {"id": (date + datetime.timedelta(days=2)).strftime("%d-%m-20%y")})
+    tarson = page.find_all("div", {"id": (date + datetime.timedelta(days=3)).strftime("%d-%m-20%y")})
+    charson = page.find_all("div", {"id": (date + datetime.timedelta(days=4)).strftime("%d-%m-20%y")})
+    pacharson = page.find_all("div", {"id": (date + datetime.timedelta(days=5)).strftime("%d-%m-20%y")})
+    sarson = page.find_all("div", {"id": (date + datetime.timedelta(days=6)).strftime("%d-%m-20%y")})
+    satarson = page.find_all("div", {"id": (date + datetime.timedelta(days=7)).strftime("%d-%m-20%y")})
+    lst = []
+    for match in aaj:
+        time = match.find("div", {"class":"scr_dt-red"}).get_text()
+        res = {
+            "Series" : match["data-series"],
+            "Team1" : match["data-teama"],
+            "Team2" : match["data-teamb"],
+            "Stadium" : match["data-venue"],
+            "time" : time
+            }
+        lst.append(res)
+
+    for match in kal:
+        time = match.find("div", {"class":"scr_dt-red"}).get_text()
+        res = {
+            "Series" : match["data-series"],
+            "Team1" : match["data-teama"],
+            "Team2" : match["data-teamb"],
+            "Stadium" : match["data-venue"],
+            "time" : time
+            }
+        lst.append(res)
+    for match in parson:
+        time = match.find("div", {"class":"scr_dt-red"}).get_text()
+        res = {
+            "Series" : match["data-series"],
+            "Team1" : match["data-teama"],
+            "Team2" : match["data-teamb"],
+            "Stadium" : match["data-venue"],
+            "time" : time
+            }
+        lst.append(res)
+    for match in tarson:
+        time = match.find("div", {"class":"scr_dt-red"}).get_text()
+        res = {
+            "Series" : match["data-series"],
+            "Team1" : match["data-teama"],
+            "Team2" : match["data-teamb"],
+            "Stadium" : match["data-venue"],
+            "time" : time
+            }
+        lst.append(res)
+    for match in charson:
+        time = match.find("div", {"class":"scr_dt-red"}).get_text()
+        res = {
+            "Series" : match["data-series"],
+            "Team1" : match["data-teama"],
+            "Team2" : match["data-teamb"],
+            "Stadium" : match["data-venue"],
+            "time" : time
+            }
+        lst.append(res)
+
+    for match in pacharson:
+        time = match.find("div", {"class":"scr_dt-red"}).get_text()
+        res = {
+            "Series" : match["data-series"],
+            "Team1" : match["data-teama"],
+            "Team2" : match["data-teamb"],
+            "Stadium" : match["data-venue"],
+            "time" : time
+            }
+        lst.append(res)
+
+
+    for match in sarson:
+        time = match.find("div", {"class":"scr_dt-red"}).get_text()
+        res = {
+            "Series" : match["data-series"],
+            "Team1" : match["data-teama"],
+            "Team2" : match["data-teamb"],
+            "Stadium" : match["data-venue"],
+            "time" : time
+            }
+        lst.append(res)
+
+    for match in satarson:
+        time = match.find("div", {"class":"scr_dt-red"}).get_text()
+        res = {
+            "Series" : match["data-series"],
+            "Team1" : match["data-teama"],
+            "Team2" : match["data-teamb"],
+            "Stadium" : match["data-venue"],
+            "time" : time
+            }
+        lst.append(res)
 
 
 @app.get("/cricket2")
